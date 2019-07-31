3d441639abe02f3e77a1e4c401aed62213a07e61
iamshiwei <2587159769@qq.com>
2019-07-31 23:08:20 +0800

    node项目路由添加

--- 8.nodejs2/manager/config/config.dev.js
+++ 8.nodejs2/manager/config/config.dev.js
@@ -1,8 +1,14 @@
+const path = require('path');
 module.exports = {
     // database
     DB_HOST: 'localhost',
     DB_PORT: 3306,
     DB_USER: 'root',
     DB_PASSWORD: 'shiwei19910513',
-    DB_NAME: '20190728'
+    DB_NAME: '20190728',
+
+    // http
+    HTTP_PORT: 8888,
+    HTTP_ROOT: path.resolve(__dirname, '../static/'),
+    HTTP_UPLOAD: path.resolve(__dirname, '../static/upload')
 }
---
+++ 8.nodejs2/manager/libs/http.js
@@ -0,0 +1,75 @@
+const http = require('http');
+const fs = require('fs');
+const querystring = require('querystring');
+const url = require('url');
+const multiparty = require('multiparty');
+const {Form} = require('zlib');
+const zlib = require('zlib');
+const router = require('../libs/router');
+const {HTTP_PORT, HTTP_ROOT, HTTP_UPLOAD} = require('../config');
+http.createServer((req, res) => {
+    let {pathname, query} = url.parse(req.url, true);
+    // 1.解析数据
+    if (req.method == 'POST') {
+        if (req.headers['content-type'].startsWith('application/x-www-form-urlencoded')) {
+            // 普通post
+            let arr = [];
+            req.on('data', buffer => {
+                arr.push(buffer);
+            })
+            req.on('end', () => {
+                let post = querystring(Buffer.concat(arr).toString());
+                // 找路由
+                handle(req.method, req.url, post, {})
+            })
+        } else {
+            // 文件
+            let form = new Form({
+                uploadDir: HTTP_UPLOAD
+            })
+            form.parse(req);
+            let post = {};
+            let files = {};
+            form.on('field', (name, value) => {
+                post[name] = value;
+            })
+            form.on('file', (name, file) => {
+                files[name] = file;
+            })
+            form.on('error', err => {
+                console.log(err);
+            })
+            form.on('close', () => {
+                // 找路由
+                handle(req.method, pathname, query, post, files);
+            })
+        }
+    } else {
+        // 找路由
+        handle(req.method, pathname, {}, {})
+    }
+
+    function handle(method, url, get, post, files) {
+        let fn = router.findRouter(method, url);
+        if(!fn) {
+            // 文件
+            let filepath = HTTP_UPLOAD+pathname;
+            fs.stat(filepath, (err, stat)=>{
+                if(err){
+                    res.writeHead(404);
+                    res.write('not found');
+                    res.end();
+                } else {
+                    let rs = fs.createReadStream(filepath);
+                    let gz = zlib.createGzip();
+                    rs.on('error', (err)=>{console.log(err)})
+                    res.setHeader('content-encoding', 'gzip');
+                    rs.pipe(gz).pipe(res);
+                }
+            })
+        } else {
+            // 接口
+        }
+    }
+}).listen(HTTP_PORT);
+console.log(`server started at ${HTTP_PORT}`);
